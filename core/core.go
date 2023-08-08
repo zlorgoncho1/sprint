0885d9768e487aa0f62756961b90195b23f44b47
zlorgoncho1 <papamatardiop3@gmail.com>
2023-08-08 02:14:26 +0000

    - NEW PACKAGE CORE: Creation de nouvelle structure pour l'utilisation du Framework: 	[Module, Controller, Route, Request, Response] - Transformation du Buffer HTTP en Struct Request - Mise à jour du module name pour l'utilisation de test
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	new file:   core/core.go
    	modified:   go.mod
    	modified:   server/server.go

---
+++ core/core.go
@@ -0,0 +1,33 @@
+package core
+
+type Module struct {
+	Name        string
+	Imports     []Module
+	Exports     []Module
+	Controllers []Controller
+}
+
+type Controller interface {
+	Routes() []Route
+	Init() (string, string)
+}
+
+type Route struct {
+	Method   string
+	Endpoint string
+	Function func(request Request) Response
+}
+
+type Request struct {
+	Method   string
+	Endpoint string
+	Protocol string
+	Headers  map[string]string
+	Query    []string
+	Body     interface{}
+	Key      string
+}
+
+type Response struct {
+	Content interface{}
+}
--- server/server.go
+++ server/server.go
@@ -1,20 +1,28 @@
 package server
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
 	"net"
-	logger "sprint/Logger"
+	"sprint/core"
+	"sprint/logger"
+
+	"strings"
 	"time"
 )
 
 type Server struct {
-	Host string
-	Port string
+	Host   string
+	Port   string
+	routes map[string]core.Route
 }
 
 var __logger logger.Logger = logger.Logger{}
 
-func (server Server) Start() (net.Listener, error) {
+func (server Server) Start(mainModule core.Module) (net.Listener, error) {
+	__logger.Log("Starting Sprint Application ...", "ServerCore")
+	server.RoutesResolver(mainModule.Controllers)
 	startTime := time.Now()
 	addr := server.Host + ":" + server.Port
 	listener, err1 := net.Listen("tcp", addr)
@@ -22,19 +30,108 @@ func (server Server) Start() (net.Listener, error) {
 		fmt.Println("Server never started")
 	}
 	endTime := time.Now()
-	__logger.Plog("Sprint application successfully started", endTime.Sub(startTime), "SprintApplication", "0", "OK")
-	__logger.Log(fmt.Sprintf("Listenning on http://%s:%s", server.Host, server.Port), "SprintApplication")
+	__logger.Plog("Sprint application successfully started", endTime.Sub(startTime), "ServerCore", "0", "OK")
+	__logger.Log(fmt.Sprintf("Listenning on http://%s:%s", server.Host, server.Port), "ServerCore")
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
 			fmt.Println("Erreur lors de l'acceptation d'une connexion :", err)
 			continue
 		}
-		go readBuffer(conn)
+		go server.readBuffer(conn)
 	}
 }
 
-func readBuffer(conn net.Conn) {
+func (server Server) RoutesResolver(controllers []core.Controller) {
+	for i := 0; i < len(controllers); i++ {
+		controller := (controllers)[i]
+		controllerName, controllerPath := controller.Init()
+		__logger.Log(fmt.Sprintf("%s | %s", controllerName, controllerPath), "ControllerResolver")
+		routes := controller.Routes()
+		server.routes = make(map[string]core.Route)
+		for o := 0; o < len(routes); o++ {
+			startTime := time.Now()
+			route := routes[o]
+			if strings.HasSuffix(controllerPath, "/") && strings.HasPrefix(route.Endpoint, "/") {
+				controllerPath = controllerPath[:len(controllerPath)-1]
+			}
+			route.Endpoint = fmt.Sprintf("%s%s", controllerPath, route.Endpoint)
+			//TODO: NEED TO CHECK PARAMS OF VIEWS
+			server.routes[route.Endpoint] = route
+			endTime := time.Now()
+			__logger.Plog(fmt.Sprintf("Mapped %s, {{ %s }}", route.Method, route.Endpoint), endTime.Sub(startTime), "ViewResolver", "0", "OK")
+		}
+	}
+}
+
+func (server Server) extractHeadData(head string) (string, string, string, map[string]string, []string, error) {
+	headParts := strings.Split(head, "\n")
+
+	requestLine := strings.Split(headParts[0], " ")
+	if len(requestLine) < 3 {
+		return "", "", "", nil, nil, errors.New("EN TETE HTTP INCORRECT")
+	}
+
+	method := requestLine[0]
+	_endpoint := requestLine[1]
+	protocol := requestLine[2]
+
+	endpointParts := strings.SplitN(_endpoint, "?", 2)
+	endpoint := endpointParts[0]
+
+	var query []string
+	if len(endpointParts) > 1 {
+		query = strings.Split(endpointParts[1], "&")
+	}
+
+	headers := make(map[string]string)
+	for _, unFormattedHeader := range headParts[1:] {
+		headerParts := strings.SplitN(unFormattedHeader, ": ", 2)
+		if len(headerParts) < 2 {
+			return "", "", "", nil, nil, errors.New("header format is incorrect")
+		}
+		headers[headerParts[0]] = headerParts[1]
+	}
+	return method, endpoint, protocol, headers, query, nil
+}
+
+func (server Server) extractHTTPBufferData(data string) (core.Request, error) {
+	formatedData := strings.ReplaceAll(data, "\x00", "")
+	formatedData = strings.ReplaceAll(formatedData, "\r", "")
+	parts := strings.Split(formatedData, "\n\n")
+	if len(parts) == 0 {
+		return core.Request{}, errors.New("FORMAT DU BUFFER INVALIDE")
+	}
+	head := parts[0]
+	var body string
+	if len(parts) == 2 {
+		body = parts[1]
+	}
+	method, endpoint, protocol, headers, query, err := server.extractHeadData(head)
+	if err != nil {
+		return core.Request{}, err
+	}
+	contentType, keyExists := headers["Content-Type"]
+	if keyExists {
+		if strings.HasPrefix(contentType, "text/plain") {
+			// do nothing
+		} else if strings.HasPrefix(contentType, "application/json") {
+			var jsonObj interface{}
+			err := json.Unmarshal([]byte(body), &jsonObj)
+			if err != nil {
+				return core.Request{}, err
+			}
+			body = jsonObj.(string)
+		} else {
+			return core.Request{}, errors.New("ContentTypeException")
+		}
+	}
+
+	key := method + endpoint
+	return core.Request{Method: method, Endpoint: endpoint, Protocol: protocol, Headers: headers, Query: query, Body: body, Key: key}, nil
+}
+
+func (server Server) readBuffer(conn net.Conn) {
 	defer conn.Close()
 	var req []byte
 	keep_loop := true
@@ -50,5 +147,9 @@ func readBuffer(conn net.Conn) {
 	for _, ch := range req {
 		msg += string(ch)
 	}
-	fmt.Println(msg)
+	_, err := server.extractHTTPBufferData(msg)
+	if err != nil {
+		__logger.Error(string(err.Error()), "ServerCore")
+	}
+	// ACTUALLY WE'RE HERE !
 }
